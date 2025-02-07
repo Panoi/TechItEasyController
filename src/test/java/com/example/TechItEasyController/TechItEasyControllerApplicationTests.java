f3c135705e23da66cf7b1675fb35511110df8910
panoi <pascal_blom9@hotmail.com>
2025-02-07 15:17:24 +0100

    initial commit

---
+++ src/test/java/com/example/TechItEasyController/TechItEasyControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TechItEasyController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasyControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
